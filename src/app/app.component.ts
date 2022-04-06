e0918eb226f4bbc4ef768920b3471000d51c32f7
alex-fourmy <alex.fourmy@pagesuite.com>
2022-04-06 16:55:43 +0100

    Added navigation with Property binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'food-recipe-ng';
+
+  loadedFeature = 'recipe'
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature
+  }
 }
