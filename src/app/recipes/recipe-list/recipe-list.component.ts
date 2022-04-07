373816e0947fd1f992bef68a40a90a48d2e8b704
alex-fourmy <alex.fourmy@pagesuite.com>
2022-04-07 15:22:39 +0100

    worked on the shopping list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {Recipe} from '../recipe.model'
 
 @Component({
@@ -8,6 +8,8 @@ import {Recipe} from '../recipe.model'
 })
 
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chicken_pasta_bake-bb82ba6.jpg?quality=90&resize=500,454'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chicken_pasta_bake-bb82ba6.jpg?quality=90&resize=500,454')
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
